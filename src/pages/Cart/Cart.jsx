b85c89a7a5dae19fd984a143149330ab1eb6eea3
Ashutosh <ashutoshmmishra15@gmail.com>
2025-09-17 05:16:18 +0530

    First

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,13 @@
-import React, { useContext } from "react";
+import React, { use, useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
-
+  
+  const navigate = useNavigate();
   return (
     <div className="cart">
       <div className="cart-items">
@@ -59,7 +61,7 @@ const Cart = () => {
             </div>
             <hr />
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
